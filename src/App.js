2f44a2a537e78c8676f892f6b843e7de22814e0c
Parth Panchal <pr930523@dal.ca>
2020-07-15 13:47:35 -0300

    "data processing module (+word cloud visulaization)"

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Register} from './components/register/Register'
 import {Login} from './components/login/Login'
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import { TwoFactorAuth } from './components/two-factor-auth/TwoFactorAuth';
+import { WordCloudGenerate } from './components/dataprocessing/Wordcloud-render';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/register" component={Register}/>
         <Route exact path="/" component={Login}/>
         <Route path="/auth" component={TwoFactorAuth}/>
+        <Route path="/word-cloud" component={WordCloudGenerate}/>
       </Switch>
       </Router>
     </div>
---
+++ src/components/dataprocessing/Wordcloud-render.jsx
@@ -0,0 +1,66 @@
+
+
+import WordCloud from "react-d3-cloud";
+import React, { useState } from 'react';
+
+export const WordCloudGenerate = () => {
+    let [dataSource,setDataSource]  = useState([]);
+
+
+        let loading =  true;
+
+    const data = [
+        { text: "Hey", value: 1000 },
+        { text: "lol", value: 200 },
+        { text: "first impression", value: 800 },
+        { text: "very cool", value: 1000000 },
+        { text: "very hot", value: 2000000 },
+        { text: "duck", value: 10 }
+    ];
+
+
+    fetch("https://wordclouddatafeed-oeueo7ct2a-uc.a.run.app")
+        .then(response => response.json())
+        .then((responseJson) => {
+            console.log('getting data from fetch', responseJson)
+
+            setTimeout(() => {
+
+                loading = false;
+                setDataSource(dataSource=responseJson);
+            }, 2000)
+
+        })
+        .catch(error => console.log(error))
+    //console.log('data',dataSource)
+
+    var dataSourceJSONFormat = [];
+    var dataSourceJSONFormat1 = [];
+    dataSource.forEach(myFunction);
+
+    function myFunction(value, index, array) {
+        var object = {};
+        object.text = array[index][0];
+        object.value =  array[index][1];
+
+        dataSourceJSONFormat1.push(object);
+        const element = [];
+        var text = "text"+":"+array[index][0];
+        text = JSON.stringify(text);
+        element.push(text);
+        var valueText = "value"+":"+array[index][1];
+        valueText = JSON.stringify(valueText);
+        element.push(valueText);
+        dataSourceJSONFormat.push(element);
+    }
+   // console.log(dataSourceJSONFormat);
+    console.log(dataSourceJSONFormat1);
+    const fontSizeMapper = word => Math.log2(word.value*10) * 5;
+    const rotate = word => word.value*10 % 360;
+    return (<WordCloud data={dataSourceJSONFormat1} fontSizeMapper={fontSizeMapper} rotate={rotate} />
+         );
+
+
+}
+
+
