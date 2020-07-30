f744b85e1a5e8a36f47d686980e8c2d700259faa
AmeliaHernandez <am629736@dal.ca>
2020-07-30 15:55:03 -0300

    online assistant module code

---
+++ Online Assistant Module/build/precache-manifest.85e775da421aa86cebf7eb04a2db8629.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd2928626bac05f3d0045f68ac2b5e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43203b87dcbc548e0039",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "769d5c1d46a43bd1ff05",
+    "url": "/static/js/2.8ebf262c.chunk.js"
+  },
+  {
+    "revision": "2484cd6bcea4995b92934619b09cb02d",
+    "url": "/static/js/2.8ebf262c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43203b87dcbc548e0039",
+    "url": "/static/js/main.6109bf46.chunk.js"
+  },
+  {
+    "revision": "9b6f902c645ea25d806d",
+    "url": "/static/js/runtime-main.476606b2.js"
+  }
+]);
